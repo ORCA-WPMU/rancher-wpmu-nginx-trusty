6f7877c4249c6ab54ee01f1a049c4a15dbbdc006
Morgan Blackthorne <morgan@windsofstorm.net>
2016-12-17 12:46:30 -0800

    Adjust php-fpm pool config

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+
+if [ "x$1" == "x" ]; then
+  CACHE="--no-cache"
+else
+  CACHE=""
+fi
+
 DATE=$(date +%Y%m%d.%H%M)
 echo "Building docker-wpmu-nginx:trusty.$DATE"
-docker build --no-cache -t="stormerider/docker-wpmu-nginx:trusty.$DATE" .
+docker build $CACHE -t="stormerider/docker-wpmu-nginx:trusty.$DATE" .
 echo ""
 echo "Building docker-wpmu-nginx:latest"
 docker build -t="stormerider/docker-wpmu-nginx:latest" .
 echo ""
 #echo "Building docker-wpmu-nginx:xenial.$DATE"
-#docker build --no-cache -t="stormerider/docker-wpmu-nginx:xenial.$DATE" . -f Dockerfile-xenial
+#docker build $CACHE -t="stormerider/docker-wpmu-nginx:xenial.$DATE" . -f Dockerfile-xenial
 #echo ""
 #echo "Building docker-wpmu-nginx:xenial"
 #docker build -t="stormerider/docker-wpmu-nginx:xenial" . -f Dockerfile-xenial
