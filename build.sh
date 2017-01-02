2ee9206a423a2513746c751ea383a341f3a531f3
Morgan Blackthorne <morgan@windsofstorm.net>
2017-01-02 03:00:13 -0800

    Re-enable xenial builds

--- build.sh
+++ build.sh
@@ -8,25 +8,28 @@ fi
 
 DATE=$(date +%Y%m%d.%H%M)
 echo "Building docker-wpmu-nginx:trusty.$DATE"
-docker build $CACHE -t="stormerider/docker-wpmu-nginx:trusty.$DATE" .
+docker build $CACHE -t="stormerider/docker-wpmu-nginx:trusty.$DATE" . && \
+echo "" && \
+echo "Pushing docker-wpmu-nginx:trusty.$DATE" && \
+docker push stormerider/docker-wpmu-nginx:trusty.$DATE
+
 echo ""
 echo "Building docker-wpmu-nginx:latest"
-docker build -t="stormerider/docker-wpmu-nginx:latest" .
-echo ""
-#echo "Building docker-wpmu-nginx:xenial.$DATE"
-#docker build $CACHE -t="stormerider/docker-wpmu-nginx:xenial.$DATE" . -f Dockerfile-xenial
-#echo ""
-#echo "Building docker-wpmu-nginx:xenial"
-#docker build -t="stormerider/docker-wpmu-nginx:xenial" . -f Dockerfile-xenial
-#echo ""
-echo "Pushing docker-wpmu-nginx:trusty.$DATE"
-docker push stormerider/docker-wpmu-nginx:trusty.$DATE
-echo ""
-echo "Pushing docker-wpmu-nginx:latest"
+docker build -t="stormerider/docker-wpmu-nginx:latest" . && \
+echo "" && \
+echo "Pushing docker-wpmu-nginx:latest" && \
 docker push stormerider/docker-wpmu-nginx:latest
+
 echo ""
-#echo "Pushing docker-wpmu-nginx:xenial.$DATE"
-#docker push stormerider/docker-wpmu-nginx:xenial.$DATE
-#echo ""
-#echo "Pushing docker-wpmu-nginx:xenial"
-#docker push stormerider/docker-wpmu-nginx:xenial
+echo "Building docker-wpmu-nginx:xenial.$DATE"
+docker build $CACHE -t="stormerider/docker-wpmu-nginx:xenial.$DATE" . -f Dockerfile-xenial && \
+echo "" && \
+echo "Pushing docker-wpmu-nginx:xenial.$DATE" && \
+docker push stormerider/docker-wpmu-nginx:xenial.$DATE
+
+echo ""
+echo "Building docker-wpmu-nginx:xenial"
+docker build -t="stormerider/docker-wpmu-nginx:xenial" . -f Dockerfile-xenial && \
+echo "" && \
+echo "Pushing docker-wpmu-nginx:xenial" && \
+docker push stormerider/docker-wpmu-nginx:xenial
